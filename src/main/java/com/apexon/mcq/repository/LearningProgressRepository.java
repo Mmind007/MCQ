034a96b74ddf96aa5151fd906c4b112462ff8758
Madhushree.Mangi <maggishiburoy@gmail.com>
2025-05-27 14:08:45 +0530

    US2.1

--- src/main/java/com/apexon/mcq/repository/LearningProgressRepository.java
+++ src/main/java/com/apexon/mcq/repository/LearningProgressRepository.java
@@ -4,11 +4,20 @@ import com.apexon.mcq.entity.LearningProgress;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface LearningProgressRepository extends JpaRepository<LearningProgress, Long> {
-    List<LearningProgress> findByUserIdOrderByDateAsc(Long userId);
+    List<LearningProgress> findByUserId(Long userId);
 
-    List<LearningProgress> findByUserIdAndAreaIdOrderByDateAsc(Long userId, Long areaId);
+    List<LearningProgress> findByUserIdAndAreaId(Long userId, Long areaId);
+
+    List<LearningProgress> findByUserIdAndDate(Long userId, LocalDate date);
+
+    //range
+    Optional<LearningProgress> findTopByUserIdAndAreaIdOrderByDateDesc(Long userId, Long areaId);
+
+    boolean existsByUserIdAndAreaIdAndDate(Long userId, Long areaId, LocalDate date);
 }
